1ae5f31feacc0c665c82346801b027c63962a3a4
PetarVancev <60193096+PetarVancev@users.noreply.github.com>
2023-08-20 19:51:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["https://sparkling-parka-hare.cyclic.cloud"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
